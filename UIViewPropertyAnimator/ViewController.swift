805156d354ed71864bc66ab48f3a0660b9b93871
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-02-15 20:19:51 +1300

    Initial Commit

---
+++ UIViewPropertyAnimator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIViewPropertyAnimator
+//
+//  Created by Daniel Wallace on 15/02/17.
+//  Copyright © 2017 Daniel Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
